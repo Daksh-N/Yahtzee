a1fb31de8af38da73948e3c90775c0a4925b7a18
Daksh <dnakra@bsd220.org>
2022-12-12 10:15:46 -0600

    Initial Commit

---
+++ Yahtzee/YahtzeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YahtzeeApp.swift
+//  Yahtzee
+//
+//  Created by Daksh Nakra on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct YahtzeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
