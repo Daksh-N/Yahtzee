4698cbd4c870ed956309c47d4e0d6e30e087c575
Daksh <dnakra@bsd220.org>
2022-12-13 10:06:25 -0600

    Got an outline going for the 2nd and 3rd views

--- Yahtzee/InstructionsView.swift
+++ Yahtzee/InstructionsView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct InstructionsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.green.opacity(0.7).ignoresSafeArea()
+            VStack {
+                CustomText(text: "Instructions")
+            }
+        }
     }
 }
 
