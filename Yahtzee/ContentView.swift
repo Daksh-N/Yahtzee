86c74da0f919bf34f97577b4c14059d432fc4bcf
Daksh <dnakra@bsd220.org>
2022-12-16 18:58:11 -0600

    Moved Calculating methods and added more comments

--- Yahtzee/ContentView.swift
+++ Yahtzee/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     // These are the dice values
-    @State private var randomValues = [0, 0, 0, 0, 0]
+    @State private var diceValues = [0, 0, 0, 0, 0]
     // These are the dice animation values
     @State private var rotationValues = [0.0, 0.0, 0.0, 0.0, 0.0]
     // These are the values that tell if a dice is being holded or not
@@ -17,8 +17,20 @@ struct ContentView: View {
     // This is the number of rolls left- self explanatory
     @State private var numberOfRolls = 3
     // These are the Scores that the player earns, not the scores calculated from what the player earns
-    @State private var scores = [0, 0, 0, 0, 0, 0,
-                                 0, 0, 0, 0, 0, 0, 0, 0]
+    @State private var scores = [0, // i=0 | Aces
+                                 0, // i=1 | Twos
+                                 0, // i=2 | Threes
+                                 0, // i=3 | Fours
+                                 0, // i=4 | Fives
+                                 0, // i=5 | Sixes
+                                 0, // i=6 | 3 of a kind
+                                 0, // i=7 | 4 of a kind
+                                 0, // i=8 | Full House
+                                 0, // i=9 | Small Straight
+                                 0, // i=10 | Large Straight
+                                 0, // i=11 | Yahtzee
+                                 0, // i=12 | Chance
+                                 0] // i=13 | Yahtzee Bonuses
     // When the player writes a score, this variable holds the value
     @State private var selectedScore = 0
     // Since the scores are in an array, we also need the index
@@ -38,7 +50,7 @@ struct ContentView: View {
                     CustomText(text: "Yahtzee!")
                     ForEach (0..<5){ i in
                         HStack {
-                            Image("pips \(randomValues[i])")
+                            Image("pips \(diceValues[i])")
                                 .resizable()
                                 .frame(width: 75, height: 75, alignment: .center)
                                 .rotationEffect(.degrees(rotationValues[i]))
@@ -65,6 +77,9 @@ struct ContentView: View {
                             .buttonStyle(CustomButtonStyle(holdValue: holdValues[i]))
                         }
                     }
+                    
+                    
+                    
                     HStack{
                         Button("Roll (\(numberOfRolls))") {
                             if numberOfRolls > 0 {
@@ -93,7 +108,7 @@ struct ContentView: View {
     func chooseRandom(times: Int, i: Int) {
         if times > 0 {
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                randomValues[i] = Int.random(in: 1...6)
+                diceValues[i] = Int.random(in: 1...6)
                 chooseRandom(times: times - 1, i: i)
             }
         }
@@ -113,9 +128,328 @@ struct ContentView: View {
     
     func reset() {
         //Resets the game
-        randomValues = [0, 0, 0, 0, 0]
+        diceValues = [0, 0, 0, 0, 0]
         numberOfRolls = 3
         holdValues = ["Hold", "Hold", "Hold", "Hold", "Hold"]
+    }
+    
+    func runCalculations() {
+        scores[0] = calcDiceNumber(number: 1)
+        scores[1] = calcDiceNumber(number: 2)
+        scores[2] = calcDiceNumber(number: 3)
+        scores[3] = calcDiceNumber(number: 4)
+        scores[4] = calcDiceNumber(number: 5)
+        scores[5] = calcDiceNumber(number: 6)
+        scores[6] = calc3OfAKind()
+        scores[7] = calc4OfAKind()
+        scores[8] = calcFullHouse()
+        scores[9] = calcSmallStraight()
+        scores[10] = calcLargeStraight()
+        scores[11] = calcYahtzee()
+        scores[12] = calcChance()
+        scores[13] = calcYahtzeeBonuses()
+    }
+    
+    func calcDiceNumber(number: Int) -> Int {
+        var count = 0
+        for i in (0..<5) {
+            if diceValues[i] == number {
+                count += diceValues[i]
+            }
+        }
+        return count
+    }
+    
+    func calc3OfAKind() -> Int {
+        var amountOfOnes = 0
+        var amountOfTwos = 0
+        var amountOfThrees = 0
+        var amountOfFours = 0
+        var amountOfFives = 0
+        var amountOfSixes = 0
+        var sum = 0
+        for i in (0..<5) {
+            sum += diceValues[i]
+        }
+        for i in (0..<5) {
+            if diceValues[i] == 1
+            {
+                amountOfOnes += 1
+            }
+            if diceValues[i] == 2
+            {
+                amountOfTwos += 1
+            }
+            if diceValues[i] == 3
+            {
+                amountOfThrees += 1
+            }
+            if diceValues[i] == 4
+            {
+                amountOfFours += 1
+            }
+            if diceValues[i] == 5
+            {
+                amountOfFives += 1
+            }
+            if diceValues[i] == 6
+            {
+                amountOfSixes += 1
+            }
+        }
+        let amountOfDice = [amountOfOnes, amountOfTwos, amountOfThrees, amountOfFours, amountOfFives, amountOfSixes]
+        for i in (0..<6) {
+            if amountOfDice[i] >= 3 {
+                return sum
+            }
+        }
+        return 0
+    }
+    
+    func calc4OfAKind() -> Int {
+        var amountOfOnes = 0
+        var amountOfTwos = 0
+        var amountOfThrees = 0
+        var amountOfFours = 0
+        var amountOfFives = 0
+        var amountOfSixes = 0
+        var sum = 0
+        for i in (0..<5) {
+            sum += diceValues[i]
+        }
+        for i in (0..<5) {
+            if diceValues[i] == 1
+            {
+                amountOfOnes += 1
+            }
+            if diceValues[i] == 2
+            {
+                amountOfTwos += 1
+            }
+            if diceValues[i] == 3
+            {
+                amountOfThrees += 1
+            }
+            if diceValues[i] == 4
+            {
+                amountOfFours += 1
+            }
+            if diceValues[i] == 5
+            {
+                amountOfFives += 1
+            }
+            if diceValues[i] == 6
+            {
+                amountOfSixes += 1
+            }
+        }
+        let amountOfDice = [amountOfOnes, amountOfTwos, amountOfThrees, amountOfFours, amountOfFives, amountOfSixes]
+        for i in (0..<6) {
+            if amountOfDice[i] >= 4 {
+                return sum
+            }
+        }
+        return 0
+    }
+    
+    func calcFullHouse() -> Int {
+        var amountOfOnes = 0
+        var amountOfTwos = 0
+        var amountOfThrees = 0
+        var amountOfFours = 0
+        var amountOfFives = 0
+        var amountOfSixes = 0
+        var sum = 0
+        for i in (0..<5) {
+            sum += diceValues[i]
+        }
+        for i in (0..<5) {
+            if diceValues[i] == 1
+            {
+                amountOfOnes += 1
+            }
+            if diceValues[i] == 2
+            {
+                amountOfTwos += 1
+            }
+            if diceValues[i] == 3
+            {
+                amountOfThrees += 1
+            }
+            if diceValues[i] == 4
+            {
+                amountOfFours += 1
+            }
+            if diceValues[i] == 5
+            {
+                amountOfFives += 1
+            }
+            if diceValues[i] == 6
+            {
+                amountOfSixes += 1
+            }
+        }
+        let amountOfDice = [amountOfOnes, amountOfTwos, amountOfThrees, amountOfFours, amountOfFives, amountOfSixes]
+        var indexToSkip = 0
+        var trioExists = false
+        var duoExists = false
+        for i in (0..<6) {
+            if amountOfDice[i] >= 3 {
+                trioExists = true
+                indexToSkip = i
+                break
+            }
+        }
+        for i in (0..<6) {
+            if i == indexToSkip {
+                //do nothing
+            }
+            else if amountOfDice[i] >= 2 {
+                duoExists = true
+                break
+            }
+        }
+        if(trioExists == true && duoExists == true)
+        {
+            return 25
+        }
+        return 0
+    }
+    
+    func calcSmallStraight() -> Int {
+        var areDicePresent = [false, false, false, false, false, false]
+        for i in (0..<5) {
+            if diceValues[i] == 1
+            {
+                areDicePresent[1-1] = true
+            }
+            if diceValues[i] == 2
+            {
+                areDicePresent[2-1] = true
+            }
+            if diceValues[i] == 3
+            {
+                areDicePresent[3-1] = true
+            }
+            if diceValues[i] == 4
+            {
+                areDicePresent[4-1] = true
+            }
+            if diceValues[i] == 5
+            {
+                areDicePresent[5-1] = true
+            }
+            if diceValues[i] == 6
+            {
+                areDicePresent[6-1] = true
+            }
+        }
+        var isSmallStraight = true
+        for i in (0..<4) {
+            if areDicePresent[i] == false {
+                isSmallStraight = false
+            }
+        }
+        if isSmallStraight == true {
+            return 30
+        }
+        isSmallStraight = true
+        for i in (1..<5) {
+            if areDicePresent[i] == false {
+                isSmallStraight = false
+            }
+        }
+        if isSmallStraight == true {
+            return 30
+        }
+        isSmallStraight = true
+        for i in (2..<6) {
+            if areDicePresent[i] == false {
+                isSmallStraight = false
+            }
+        }
+        if isSmallStraight == true {
+            return 30
+        }
+        return 0
+    }
+    
+    func calcLargeStraight() -> Int {
+        var areDicePresent = [false, false, false, false, false, false]
+        for i in (0..<5) {
+            if diceValues[i] == 1
+            {
+                areDicePresent[1-1] = true
+            }
+            if diceValues[i] == 2
+            {
+                areDicePresent[2-1] = true
+            }
+            if diceValues[i] == 3
+            {
+                areDicePresent[3-1] = true
+            }
+            if diceValues[i] == 4
+            {
+                areDicePresent[4-1] = true
+            }
+            if diceValues[i] == 5
+            {
+                areDicePresent[5-1] = true
+            }
+            if diceValues[i] == 6
+            {
+                areDicePresent[6-1] = true
+            }
+        }
+        var isLargeStraight = true
+        for i in (0..<5) {
+            if areDicePresent[i] == false {
+                isLargeStraight = false
+            }
+        }
+        if isLargeStraight == true {
+            return 40
+        }
+        isLargeStraight = true
+        for i in (1..<6) {
+            if areDicePresent[i] == false {
+                isLargeStraight = false
+            }
+        }
+        if isLargeStraight == true {
+            return 40
+        }
+        return 0
+    }
+    
+    func calcYahtzee() -> Int {
+        var isYahtzee = true
+        for d in (1...6)
+        {
+            for i in (0..<5)
+            {
+                if diceValues [i] != d
+                {
+                    isYahtzee = false
+                }
+            }
+            if isYahtzee == true {return 50}
+            isYahtzee = true
+        }
+        return 0
+    }
+    
+    func calcChance() -> Int {
+        var chance = 0
+        for i in (0..<5) {
+            chance += diceValues[i]
+        }
+        return chance
+    }
+    
+    func calcYahtzeeBonuses() -> Int {
+        return -1
     }
 }
 
